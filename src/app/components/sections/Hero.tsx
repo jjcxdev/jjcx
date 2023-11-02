44a6431bac3e2ca4cc1a8c67aa6087a8fa0b1f95
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-11-02 07:07:08 -0400

    hero text

--- src/app/components/sections/Hero.tsx
+++ src/app/components/sections/Hero.tsx
@@ -4,8 +4,8 @@ export default function Hero() {
       <div className="flex justify-between">
         <div></div>
         <div className="flex flex-col w-full items-center text-center">
-          <h1 className="font-poppins text-3xl sm:text-6xl md:text-8xl text-center font-bold uppercase text-white">
-            Justin<span className="font-light ml-1">Chambers</span>
+          <h1 className="font-poppins text-3xl sm:text-6xl md:text-8xl text-center font-extrabold uppercase text-white">
+            Justin<span className="font-thin">Chambers</span>
           </h1>
           <h2 className="font-poppins text-xl sm:text-3xl md:text-4xl text-center font-light uppercase text-white">
             Frontend Developer
