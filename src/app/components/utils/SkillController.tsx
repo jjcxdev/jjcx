1d73bfbcea11c3d8b48666ca8602ea3baf2b7629
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-11-02 02:42:07 -0400

    fix language display (#12)
    lgtm

--- src/app/components/utils/SkillController.tsx
+++ src/app/components/utils/SkillController.tsx
@@ -16,17 +16,32 @@ const SkillController: React.FC<SkillProps> = ({
   const [svgContent, setSvgContent] = useState<string | null>(null);
   const [isHovered, setIsHovered] = useState(false);
 
+  const [svgWidth, setSvgWidth] = useState<number>(0);
+  const [svgHeight, setSvgHeight] = useState<number>(0);
+
   useEffect(() => {
     // Fetch the SVG content
     fetch(icon)
       .then((response) => response.text())
       .then((content) => {
+        //Extract viewbox dimensions
+        const viewBoxMatch = content.match(
+          /viewBox="(\d+) (\d+) (\d+) (\d+)"/i
+        );
+        if (viewBoxMatch) {
+          setSvgWidth(Number(viewBoxMatch[3]));
+          setSvgHeight(Number(viewBoxMatch[4]));
+        }
         setSvgContent(content);
       });
   }, [icon]);
 
-  const radius = 46; // SVG circle radius
-  const circumference = 2 * Math.PI * radius;
+  // Determine the scaling factor based on SVG dimensions
+  const scaleFactor = Math.min(svgWidth, svgHeight) / 175;
+  const scaledRadius = 100 * scaleFactor;
+
+  //Update the circumference based on scale radius
+  const circumference = 2 * Math.PI * scaledRadius;
   const progress = ((100 - value) / 100) * circumference; // Clockwise progress
 
   const handleMouseEnter = () => {
@@ -42,13 +57,18 @@ const SkillController: React.FC<SkillProps> = ({
       className="flex flex-col items-center"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}>
-      <div className="relative md:w-28 md:h-28 w-14 h-14">
+      <div
+        className="relative"
+        style={{
+          width: `${scaledRadius * 2.25}px`,
+          height: `${scaledRadius * 2.25}px`,
+        }}>
         <svg className="absolute top-0 left-0 w-full h-full">
           {/* Clipping mask for the fill */}
           <defs>
             <clipPath id="circle-clip">
               <circle
-                r={radius}
+                r={scaledRadius}
                 cx="50%"
                 cy="50%"
                 className="fill-none"
@@ -57,7 +77,7 @@ const SkillController: React.FC<SkillProps> = ({
             </clipPath>
           </defs>
           <circle
-            r={radius}
+            r={scaledRadius}
             cx="50%"
             cy="50%"
             className="text-dark stroke-current fill-none w-full h-full"
@@ -65,7 +85,7 @@ const SkillController: React.FC<SkillProps> = ({
           />
           {/* Apply fill color and animation based on hover state */}
           <circle
-            r={radius}
+            r={scaledRadius}
             cx="50%"
             cy="50%"
             className={`text-accent stroke-current fill-none w-full h-full transform -rotate-90 origin-center transition-stroke-dashoffset duration-300 ${
@@ -78,12 +98,16 @@ const SkillController: React.FC<SkillProps> = ({
           />
         </svg>
         <div
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 md:w-12 md:h-12 w-6 h-6 flex items-center justify-center"
+          className="absolute w-12 h-12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
           dangerouslySetInnerHTML={{ __html: svgContent ? svgContent : "" }}
-          style={{ fill: color || "currentColor" }}
+          style={{
+            width: `${scaledRadius}px`,
+            height: `${scaledRadius}px`,
+            fill: color || "currentColor",
+          }}
         />
       </div>
-      <div className="font-poppins text-clamp-xs text-center">{title}</div>
+      <div className="font-poppins text-center">{title}</div>
     </div>
   );
 };
