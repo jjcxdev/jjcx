2e36a642da38b72f30ab513f53322c01d46dbc6d
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-11-02 01:25:26 -0400

    redesign footer Fixes #4

--- src/app/components/sections/Footer.tsx
+++ src/app/components/sections/Footer.tsx
@@ -1,65 +1,51 @@
 import Image from "next/image";
 import Link from "next/link";
-import { BsGithub, BsMedium } from "react-icons/bs";
+import { FaGithub, FaMedium, FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
   return (
     <>
-      <footer className="z-20 relative h-screen bg-dark text-white flex flex-col py-10 font-extralight items-center justify-center flex-wrap mx-auto w-full">
-        <div className="flex items-center py-12">
-          <Link href="/">
-            <Image
-              src="/jjcx_logo_dk.svg"
-              width={75}
-              height={20}
-              alt="jjcx logo"
-              style={{ width: "75px", height: "20px" }}
-            />
-          </Link>
-        </div>
-        <div className="h-px w-3/4 border-0 bg-gradient-to-r from-transparent via-cyan-800 to-transparent"></div>
+      <footer className="z-20 relative h-full bg-dark text-white font-space flex flex-col py-10 font-extralight items-center justify-center gap-6 flex-wrap mx-auto w-full">
+        <Link href="/">
+          <Image
+            src="/jjcx_logo.svg"
+            width={75}
+            height={20}
+            alt="jjcx logo"
+            style={{ width: "75px", height: "20px" }}
+          />
+        </Link>
 
-        <div className="flex items-center gap-6 py-12">
-          <Link className="hover:text-accent transition duration-300" href="#">
-            home
-          </Link>
-          <Link
-            className="hover:text-accent transition duration-300"
-            href="#about-section">
-            about
-          </Link>
-          <Link
-            className="hover:text-accent transition duration-300"
-            href="#work-section">
-            work
-          </Link>
-          <Link
-            className="hover:text-accent transition duration-300"
-            href="./justinchambers_resume_2023.pdf">
-            resume
-          </Link>
-        </div>
-        <div className="h-px w-3/4 border-0 bg-gradient-to-r from-transparent via-cyan-800 to-transparent "></div>
-        <div className="flex flex-col items-center py-12 text-2xl">
-          <div className="flex items-center py-6 text-2xl gap-2">
-            <div>
-              <Link
-                aria-label="Visit my GitHub"
-                className="hover:text-accent transition duration-300"
-                href="https://www.github.com/jjcxdev">
-                <BsGithub />
-              </Link>
-            </div>
-            <div>
-              <Link
-                aria-label="Read my Medium articles"
-                className="hover:text-accent transition duration-300"
-                href="https://medium.com/@jjcx">
-                <BsMedium />
-              </Link>
-            </div>
+        <div className="flex flex-row items-center gap-4">
+          <div>
+            <Link
+              aria-label="Visit my GitHub"
+              className="hover:text-accent transition duration-300"
+              href="https://www.github.com/jjcxdev">
+              <FaGithub />
+            </Link>
           </div>
-          <div className="text-sm"> © jjcx 2023</div>
+          <div>
+            <Link
+              aria-label="Visit my GitHub"
+              className="hover:text-accent transition duration-300"
+              href="https://www.github.com/jjcxdev">
+              <FaXTwitter />
+            </Link>
+          </div>
+
+          <div>
+            <Link
+              aria-label="Read my Medium articles"
+              className="hover:text-accent transition duration-300"
+              href="https://medium.com/@jjcx">
+              <FaMedium />
+            </Link>
+          </div>
+        </div>
+
+        <div className="text-xs">
+          i did this <span className="font-sans">©</span> 2023
         </div>
       </footer>
     </>
