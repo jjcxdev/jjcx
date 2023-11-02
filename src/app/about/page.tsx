46c897fd2e6df0284dd1489bebc53b3fc18f0897
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-11-02 06:00:37 -0400

    project page rest is bingo (#21)
    lgtm

--- src/app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <div role="main" aria-labelledby="course-heading">
-      <div className="bg-white text-custom-blue">About</div>
-    </div>
-  );
-}
