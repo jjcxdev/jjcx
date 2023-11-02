c2e7350cd05a3ad59cacb601581f7df3ae14f7c2
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-11-02 01:38:08 -0400

    fix text sizing (#11)
    lgtm

--- src/app/components/sections/About.tsx
+++ src/app/components/sections/About.tsx
@@ -5,15 +5,15 @@ export default function About() {
         id="about-section"
         role="region"
         aria-label="About Section"
-        className="z-20 relative text-white bg-dark flex flex-col py-10 font-extralight items-center justify-center h-screen flex-wrap w-full ">
+        className="z-20 relative text-white bg-dark flex flex-col py-10 font-extralight items-center justify-center h-full flex-wrap w-full ">
         <div className="flex flex-col w-full items-center">
           <div className="w-10/12 max-w-5xl">
             <div className="flex flex-col gap-20">
-              <div className="text-clamp-xs">
+              <div className="">
                 <h3 className="text-clamp-md font-bold uppercase items-start flex pb-4">
                   About
                 </h3>
-                <p className="text-clamp-xs">
+                <p className="">
                   Hey, I&apos;m Justin. For most of my adult life, I&apos;ve
                   been behind the camera, shooting and producing big projects in
                   film and TV. But I&apos;ve always been a tech and design
